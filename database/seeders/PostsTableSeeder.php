c209def79f86b38466a3fccefdd71cba94865e76
FedericoPiazzolla <Federicop257@gmail.com>
2024-01-23 17:48:24 +0100

    add: create resource controller

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use illuminate\Support\Str;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $post = new Post();
+            $post->title = $faker->sentence(4);
+            $post->content = $faker->text(500);
+            $post->slug = Str::slug($post->title);
+            $post->save();
+        }
+        
+    }
+}
