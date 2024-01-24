3cef4e3d59abe589d67a6d83316aaf4561a867c7
FedericoPiazzolla <Federicop257@gmail.com>
2024-01-24 11:14:30 +0100

    add: error message in input create

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,36 +8,43 @@
         <a class="btn btn-warning" href="{{ route('admin.projects.create') }}">Create a new Project</a>
       </div>
 
-      <table class="table table-striped mt-5">
+      @if (count($projects) > 0)
+         <table class="table table-striped mt-5">
 
-        <thead>
+          <thead>
 
-          <tr>
-            <th scope="col">id</th>
-            <th scope="col">Title</th>
-            <th scope="col">Data</th>
-            <th scope="col">Actions</th>
-          </tr>
-
-        </thead>
-
-        <tbody>
-
-          @foreach ($projects as $project)
             <tr>
-              <th scope="row">{{ $project->id }}</th>
-              <td>{{ $project->title }}</td>
-              <td>{{ $project->created_at }}</td>
-              <td>
-                <a class="btn btn-success" href="{{ route('admin.projects.show', ['project' => $project->slug]) }}">Details</a>
-              </td>
-            </tr>  
-          @endforeach
+              <th scope="col">id</th>
+              <th scope="col">Title</th>
+              <th scope="col">Data</th>
+              <th scope="col">Actions</th>
+            </tr>
+
+          </thead>
+
+          <tbody>
+
+            @foreach ($projects as $project)
+              <tr>
+                <th scope="row">{{ $project->id }}</th>
+                <td>{{ $project->title }}</td>
+                <td>{{ $project->created_at }}</td>
+                <td>
+                  <a class="btn btn-success" href="{{ route('admin.projects.show', ['project' => $project->slug]) }}">Details</a>
+                </td>
+              </tr>  
+            @endforeach
+            
           
-        
-        </tbody>
+          </tbody>
 
-      </table>
-
+        </table> 
+      @else
+      <div class="alert alert-warning mt-5">
+        <h2 class="text-center">You haven't entered any projects yet, click the button to add one</h2>
+      </div>
+          
+      @endif
+      
     </div>
 @endsection
